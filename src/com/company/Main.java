f7e54a92df684e9797fcb052030eaf2f02a5439a
mmm1379 <mmmasoudpour@gmail.com>
2020-03-15 18:41:03 +0330

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+
+package com.company;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner text = new Scanner(System.in);
+        String a = text.next();
+        String b = text.next();
+        System.out.println(a+b);
+
+	System.out.println("\nhello, the program worked!");
+
+    }
+}
